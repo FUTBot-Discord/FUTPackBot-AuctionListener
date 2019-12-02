0bda02755e9f392e01527c2c6b5bdb1ffea2a8a8
Tjird <tjeerd@tjird.nl>
2019-12-02 01:24:04 +0100

    sakrt

--- dist/app.js
+++ dist/app.js
@@ -64,7 +64,7 @@ function _init() {
               var _ref = (0, _asyncToGenerator2["default"])(
               /*#__PURE__*/
               _regenerator["default"].mark(function _callee(channel, message) {
-                var aInfo;
+                var aInfo, pInfo, pName;
                 return _regenerator["default"].wrap(function _callee$(_context) {
                   while (1) {
                     switch (_context.prev = _context.next) {
@@ -77,25 +77,48 @@ function _init() {
                         return _context.abrupt("return");
 
                       case 2:
-                        console.log("[e] [".concat(message, "] Expired."));
+                        console.log("[a] [".concat(message, "] Expired."));
                         _context.next = 5;
                         return getAuctionById(message);
 
                       case 5:
                         aInfo = _context.sent;
 
-                        if (!(aInfo.b_club_id == 0)) {
-                          _context.next = 9;
+                        if (!(aInfo == null)) {
+                          _context.next = 8;
                           break;
                         }
 
-                        console.log("[r] [".concat(message, "] No buyer was found."));
+                        return _context.abrupt("return");
+
+                      case 8:
+                        if (!(aInfo.b_club_id == 0)) {
+                          _context.next = 11;
+                          break;
+                        }
+
+                        console.log("[a] [".concat(message, "] No buyer was found."));
                         return _context.abrupt("return", resetTransferPlayer(message));
 
-                      case 9:
-                        console.log("[t] [".concat(message, "] Buyer(").concat(aInfo.b_club_id, ") has been found."));
+                      case 11:
+                        console.log("[a] [".concat(message, "] Buyer(").concat(aInfo.b_club_id, ") has been found."));
+                        _context.next = 14;
+                        return addCoinsToClub(aInfo.s_club_id, aInfo.current_bid);
 
-                      case 10:
+                      case 14:
+                        _context.next = 16;
+                        return auctionBuyNow(aInfo.id, aInfo.b_club_id);
+
+                      case 16:
+                        _context.next = 18;
+                        return getPlayerVersionById(aInfo.player_id);
+
+                      case 18:
+                        pInfo = _context.sent;
+                        pName = pInfo.meta_info.common_name ? pInfo.meta_info.common_name : "".concat(pInfo.meta_info.first_name, " ").concat(pInfo.meta_info.last_name);
+                        redis.publish("auctionEnd", JSON.stringify(aInfo));
+
+                      case 21:
                       case "end":
                         return _context.stop();
                     }
@@ -103,7 +126,7 @@ function _init() {
                 }, _callee);
               }));
 
-              return function (_x3, _x4) {
+              return function (_x8, _x9) {
                 return _ref.apply(this, arguments);
               };
             }());
@@ -127,13 +150,13 @@ function getAuctionById(_x) {
 function _getAuctionById() {
   _getAuctionById = (0, _asyncToGenerator2["default"])(
   /*#__PURE__*/
-  _regenerator["default"].mark(function _callee3(id) {
+  _regenerator["default"].mark(function _callee3(auction_id) {
     var query, res;
     return _regenerator["default"].wrap(function _callee3$(_context3) {
       while (1) {
         switch (_context3.prev = _context3.next) {
           case 0:
-            query = "{ getAuctionById(id: \"".concat(id, "\") { id player_id b_club_id s_club_id current_bid buy_now } }");
+            query = "{ getAuctionById(id: \"".concat(auction_id, "\") { id player_id b_club_id s_club_id current_bid start_price buy_now end_timestamp } }");
             _context3.next = 3;
             return graphql.request(query);
 
@@ -159,25 +182,146 @@ function _resetTransferPlayer() {
   _resetTransferPlayer = (0, _asyncToGenerator2["default"])(
   /*#__PURE__*/
   _regenerator["default"].mark(function _callee4(aId) {
-    var query, res;
+    var query;
     return _regenerator["default"].wrap(function _callee4$(_context4) {
       while (1) {
         switch (_context4.prev = _context4.next) {
           case 0:
             query = "mutation { resetTransferPlayer(auction_id: \"".concat(aId, "\") { id } }");
-            _context4.next = 3;
+            _context4.prev = 1;
+            _context4.next = 4;
             return graphql.request(query);
 
-          case 3:
-            res = _context4.sent;
+          case 4:
+            _context4.next = 8;
+            break;
+
+          case 6:
+            _context4.prev = 6;
+            _context4.t0 = _context4["catch"](1);
+
+          case 8:
             return _context4.abrupt("return", true);
 
-          case 5:
+          case 9:
           case "end":
             return _context4.stop();
         }
       }
-    }, _callee4);
+    }, _callee4, null, [[1, 6]]);
   }));
   return _resetTransferPlayer.apply(this, arguments);
+}
+
+function addCoinsToClub(_x3, _x4) {
+  return _addCoinsToClub.apply(this, arguments);
+}
+
+function _addCoinsToClub() {
+  _addCoinsToClub = (0, _asyncToGenerator2["default"])(
+  /*#__PURE__*/
+  _regenerator["default"].mark(function _callee5(club_id, coins) {
+    var query;
+    return _regenerator["default"].wrap(function _callee5$(_context5) {
+      while (1) {
+        switch (_context5.prev = _context5.next) {
+          case 0:
+            query = "mutation { addCoinsToClub(club_id: \"".concat(club_id, "\", coins: \"").concat(coins, "\") { id } }");
+            _context5.prev = 1;
+            _context5.next = 4;
+            return graphql.request(query);
+
+          case 4:
+            _context5.next = 10;
+            break;
+
+          case 6:
+            _context5.prev = 6;
+            _context5.t0 = _context5["catch"](1);
+            console.log(_context5.t0);
+            return _context5.abrupt("return", false);
+
+          case 10:
+            return _context5.abrupt("return", true);
+
+          case 11:
+          case "end":
+            return _context5.stop();
+        }
+      }
+    }, _callee5, null, [[1, 6]]);
+  }));
+  return _addCoinsToClub.apply(this, arguments);
+}
+
+function auctionBuyNow(_x5, _x6) {
+  return _auctionBuyNow.apply(this, arguments);
+}
+
+function _auctionBuyNow() {
+  _auctionBuyNow = (0, _asyncToGenerator2["default"])(
+  /*#__PURE__*/
+  _regenerator["default"].mark(function _callee6(auction_id, club_id) {
+    var query;
+    return _regenerator["default"].wrap(function _callee6$(_context6) {
+      while (1) {
+        switch (_context6.prev = _context6.next) {
+          case 0:
+            query = "mutation { changeTransferPlayer(club_id: \"".concat(club_id, "\", auction_id: \"").concat(auction_id, "\") { id } }");
+            _context6.prev = 1;
+            _context6.next = 4;
+            return graphql.request(query);
+
+          case 4:
+            _context6.next = 10;
+            break;
+
+          case 6:
+            _context6.prev = 6;
+            _context6.t0 = _context6["catch"](1);
+            console.log(_context6.t0);
+            return _context6.abrupt("return", false);
+
+          case 10:
+            return _context6.abrupt("return", true);
+
+          case 11:
+          case "end":
+            return _context6.stop();
+        }
+      }
+    }, _callee6, null, [[1, 6]]);
+  }));
+  return _auctionBuyNow.apply(this, arguments);
+}
+
+function getPlayerVersionById(_x7) {
+  return _getPlayerVersionById.apply(this, arguments);
+}
+
+function _getPlayerVersionById() {
+  _getPlayerVersionById = (0, _asyncToGenerator2["default"])(
+  /*#__PURE__*/
+  _regenerator["default"].mark(function _callee7(id) {
+    var query, res;
+    return _regenerator["default"].wrap(function _callee7$(_context7) {
+      while (1) {
+        switch (_context7.prev = _context7.next) {
+          case 0:
+            query = "{ getPlayerVersionById(id: ".concat(id, ") { def dri id nation_info{ img } pac pas phy meta_info{ common_name last_name first_name img } preferred_position rareflag rating sho min_price club_info{ img } } }");
+            _context7.next = 3;
+            return graphql.request(query);
+
+          case 3:
+            res = _context7.sent;
+            return _context7.abrupt("return", res.getPlayerVersionById);
+
+          case 5:
+          case "end":
+            return _context7.stop();
+        }
+      }
+    }, _callee7);
+  }));
+  return _getPlayerVersionById.apply(this, arguments);
 }
--- src/app.js
+++ src/app.js
@@ -36,23 +36,35 @@ async function init() {
 
   redis.on("message", async (channel, message) => {
     if (channel !== expired_subKey) return;
-    console.log(`[e] [${message}] Expired.`);
+    console.log(`[a] [${message}] Expired.`);
 
     let aInfo = await getAuctionById(message);
 
+    if (aInfo == null) return;
+
     if (aInfo.b_club_id == 0) {
-      console.log(`[r] [${message}] No buyer was found.`);
+      console.log(`[a] [${message}] No buyer was found.`);
       return resetTransferPlayer(message);
     }
 
-    console.log(`[t] [${message}] Buyer(${aInfo.b_club_id}) has been found.`);
+    console.log(`[a] [${message}] Buyer(${aInfo.b_club_id}) has been found.`);
+
+    await addCoinsToClub(aInfo.s_club_id, aInfo.current_bid);
+    await auctionBuyNow(aInfo.id, aInfo.b_club_id);
+
+    const pInfo = await getPlayerVersionById(aInfo.player_id);
+    const pName = pInfo.meta_info.common_name
+      ? pInfo.meta_info.common_name
+      : `${pInfo.meta_info.first_name} ${pInfo.meta_info.last_name}`;
+
+    redis.publish("auctionEnd", JSON.stringify(aInfo));
   });
 }
 
 init();
 
-async function getAuctionById(id) {
-  let query = `{ getAuctionById(id: "${id}") { id player_id b_club_id s_club_id current_bid buy_now } }`;
+async function getAuctionById(auction_id) {
+  let query = `{ getAuctionById(id: "${auction_id}") { id player_id b_club_id s_club_id current_bid start_price buy_now end_timestamp } }`;
   let res = await graphql.request(query);
 
   return res.getAuctionById;
@@ -60,7 +72,43 @@ async function getAuctionById(id) {
 
 async function resetTransferPlayer(aId) {
   let query = `mutation { resetTransferPlayer(auction_id: "${aId}") { id } }`;
-  let res = await graphql.request(query);
+
+  try {
+    await graphql.request(query);
+  } catch (e) {}
 
   return true;
+}
+
+async function addCoinsToClub(club_id, coins) {
+  let query = `mutation { addCoinsToClub(club_id: "${club_id}", coins: "${coins}") { id } }`;
+
+  try {
+    await graphql.request(query);
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+
+  return true;
+}
+
+async function auctionBuyNow(auction_id, club_id) {
+  let query = `mutation { changeTransferPlayer(club_id: "${club_id}", auction_id: "${auction_id}") { id } }`;
+
+  try {
+    await graphql.request(query);
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+
+  return true;
+}
+
+async function getPlayerVersionById(id) {
+  let query = `{ getPlayerVersionById(id: ${id}) { def dri id nation_info{ img } pac pas phy meta_info{ common_name last_name first_name img } preferred_position rareflag rating sho min_price club_info{ img } } }`;
+  let res = await graphql.request(query);
+
+  return res.getPlayerVersionById;
 }
